76645a2f45449187d638b502a73f6c635473790d
trigtbh <github@trigtbh.dev>
2025-10-18 15:27:20 -0700

    add basic infrastructure to start backend

---
+++ backend/app/api/db.py
@@ -0,0 +1,28 @@
+import dotenv
+dotenv.load_dotenv()
+
+import psycopg2
+import os
+base = os.path.dirname(os.path.realpath(__file__))
+
+conn_uri = os.environ["UNFREEZE_DB_URI"]
+
+
+try:
+    conn = psycopg2.connect(conn_uri)
+    conn.set_session(autocommit=True)
+    conn.cursor()
+except:
+    creation_uri = conn_uri
+    conn = psycopg2.connect(creation_uri)
+    conn.set_session(autocommit=True)
+    with open(os.path.join(base, "CreateDiscordDB.sql")) as f:
+        conn.cursor().execute(f.read())
+
+def connect_to_db():
+    conn = psycopg2.connect(conn_uri)
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
+    return cur
+
+cur = connect_to_db()
