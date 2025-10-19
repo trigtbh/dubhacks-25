46148be21fe55f0dfce0d9351ee521c891c7bfc9
trigtbh <lucas.varotto01@gmail.com>
2025-10-19 08:23:56 +0000

    fixed issues with prod

--- backend/main.py
+++ backend/main.py
@@ -23,4 +23,4 @@ async def homepage():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, proxy_headers=True, forwarded_allow_ips="*")
